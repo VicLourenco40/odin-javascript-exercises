5cb7bdf77a1c91883a1075080b8455a425e24452
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-10-09 16:19:45 +0100

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...valuesToRemove) {
+    for (const value of valuesToRemove) {
+        while(array.includes(value)) {
+            const index = array.lastIndexOf(value);
+            array.splice(index, 1);
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
