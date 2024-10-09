2fc47461c9fe087fd0b78d07918dc9fa6b7d5f93
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-10-09 17:26:12 +0100

    Solve 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    const isYearDivisibleByFour = !(year % 4);
+    const isYearCentury = !(year % 100);
+    const isYearDivisibleByFourHundred = !(year % 400);
+    
+    return isYearDivisibleByFour && (!isYearCentury || isYearDivisibleByFourHundred);
 };
 
 // Do not edit below this line
