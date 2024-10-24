ed2a14bb06583880ba4e95db519e8775b290229b
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-10-24 16:15:45 +0100

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    if (member < 0) return 'OOPS';
 
+    let sequence = [0, 1];
+
+    for (let i = 2; i <= member; i++) {
+        sequence[i] = sequence[i - 1] + sequence[i - 2];
+    }
+
+    return sequence[member];
 };
 
 // Do not edit below this line
