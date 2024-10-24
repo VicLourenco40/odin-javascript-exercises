7e06ac378426e3aaeb7b7a17a0317f4fdc3c2b3d
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-10-24 17:13:59 +0100

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const getAge = function (person) {
+    return (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => (
+        getAge(oldest) < getAge(person) ? person : oldest
+    ));
 };
 
 // Do not edit below this line
