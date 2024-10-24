704166a946cde26ad68a771cb0cdfb2561dd6829
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2024-10-24 15:20:22 +0100

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(num, num2) {
+  return num + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num, num2) {
+  return num - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  return nums.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((total, num) => total * num);
 };
 
-const power = function() {
-	
+const power = function(num, num2) {
+  return num ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+
+  for (let i = num; i > 1; i--) {
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
